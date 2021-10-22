d306c487fa0f3b6d354c58175408a6339e0f1261
dongmeijuan <1398670158@qq.com>
2021-10-22 11:49:32 +0800

    titlestatus

--- src/pages/ultronHeaderPage/index.jsx
+++ src/pages/ultronHeaderPage/index.jsx
@@ -1,8 +1,8 @@
-import styles from './index.css';
 import { useStyles } from '../components/makestyle';
-import { ToggleButton, ToggleButtonGroup, Toolbar, Box, AppBar, MenuIcon, IconButton, MoreIcon, StyledFab, Typography, Button } from '@material-ui/core';
+import { ToggleButton, ToggleButtonGroup, Toolbar, Box, AppBar, Divider, IconButton, MoreIcon, StyledFab, Typography, Button } from '@material-ui/core';
 import { AccessAlarm, Stop } from '@material-ui/icons';
 import { connect } from 'dva';
+import styles from './index.css';
 
 const mapStateToProps = state => {
   const ultronHeader = state['ultronHeader'];
@@ -10,6 +10,18 @@ const mapStateToProps = state => {
     ultronHeader,
   };
 };
+
+export const HeaderStatus = ({ title, textObj, flag = 1, color = '#5E5E5E' }) => {
+  return (
+    <>
+      <p style={{ paddingLeft: 20, paddingRight: 20 }}>
+        <span style={{ fontSize: 16, fontWeight: 600, fontFamily: 'initial', color: '#666666' }}>{title}</span><br />
+        <span style={{ fontSize: 18, fontWeight: 600, fontFamily: 'monospace', color: color }}> {textObj}</span>
+      </p>
+      <Divider orientation="vertical" variant="middle" flexItem />
+    </>
+  )
+}
 
 export const UltronHeader = props => {
   const { dispatch } = props;
@@ -19,21 +31,28 @@ export const UltronHeader = props => {
     setAlignment(newAlignment);
   };
 
+
+
   return (
     <h1 className={styles.title}>
       <div>
         <AppBar position="fixed" className={useStyles().headerBg}>
           <p>
             <img src="./spaceman.png" width="45" style={{ paddingLeft: 30 }}></img>
-            <span style={{ fontSize: 28, fontWeight: 500, paddingLeft: 7, fontFamily: 'fantasy', color: '#9E9E9E' }}>Ultron</span>
+            <span style={{ fontSize: 28, fontWeight: 500, paddingLeft: 7, fontFamily: 'fantasy', color: '#404040' }}>Ultron</span>
             <Toolbar className={useStyles().floatRight}>
+              <HeaderStatus title='HOST' textObj='https://www.baidu.com' />
+              <HeaderStatus title='STATUS' textObj='RUNNING(100 users)' color='#6495ED' />
+              <HeaderStatus title='RPS' textObj='22.5' />
+              <HeaderStatus title='FAILURES' textObj='10%' />
+              &nbsp;&nbsp;
               <Button variant="contained" size="large" color="error" startIcon={<Stop />}> STOP</Button>&nbsp;&nbsp;&nbsp;
               <Button variant="contained" size="large">Reset Starts</Button>
             </Toolbar>
           </p>
           <br />
         </AppBar>
-      </div>
+      </div >
     </h1 >
   );
 };
